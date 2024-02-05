34c4e24506e6815f060907fa39f9fc920210dbe5
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-05 15:50:56 +0600

    Variables update done

---
+++ variables.js
@@ -0,0 +1,21 @@
+// Variables
+
+// Primitive
+/*
+1. Numerical => num = 69;
+2. String => name = 'Prosenjith'; 
+3. Boolian => isEngineer = true;
+4. Null => no any thing difine ;
+5. Undifined => id = ; => Did not difined 
+
+// Non Primitive
+
+6. Array
+7. Object
+
+*/ 
+
+
+// Var => do not use that.
+// const => it is use just fixed values, Can not change values.
+// let => it is change values any time.
