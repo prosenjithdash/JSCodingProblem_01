5539636931a464f1e33ab900b2cb49afc7352207
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-06 13:32:02 +0600

    Loop updated

--- loop.js
+++ loop.js
@@ -1,9 +1,9 @@
 /*
-    1. for loop
-    2. while loop
-    3. do while loop
-    4. for of
-    5. break
-    6. continue
+    1. for loop => for(const i = 0; i > 50; i++){}
+    2. while loop => const i = 0; while(i > 50){console.log(i) i++;}
+    3. do while loop => 
+    4. for of => for( const num of numbers){console.log(i)}
+    5. break => condition work finish then quit program
+    6. continue => console.log with out spacepic element
     
 */
