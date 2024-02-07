78480f698521f2a6f64d54d18e5be58fd32e1d73
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-07 23:55:49 +0600

    Added math part

---
+++ math.js
@@ -0,0 +1,22 @@
+//MATH
+
+//Find Max number
+
+const max = Math.max(69, 33, 45, 22, 90);
+console.log(max)
+
+
+//Find Min number
+
+const min = Math.min(69, 33, 45, 22, 90);
+console.log(min)
+
+// Pi value
+
+const pi = Math.PI;
+console.log(pi)
+
+// abs
+
+const abs = Math.abs(5-30);
+console.log(abs)
